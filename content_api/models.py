46494953680977fa9ed539777ef840e9b61b03aa
Dieumerci Kazadi <dieumercikaz@gmail.com>
2018-04-22 16:28:50 +0200

    migrations successful, image added to DB

--- content_api/models.py
+++ content_api/models.py
@@ -8,3 +8,4 @@ from django.db import models
 class Item(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='images')
